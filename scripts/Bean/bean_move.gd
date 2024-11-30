62b3dd4a55859f5db6484cb1438fc360ea0ef175
The-Glitch-Lord <maximuswalker@yahoo.com>
2024-11-30 12:29:01 -0500

    clamp acheived

--- scripts/Bean/bean_move.gd
+++ scripts/Bean/bean_move.gd
@@ -13,18 +13,18 @@ var mouse_position:Vector2
 
 func _input(event: InputEvent) -> void:
 	
-	if Input.is_action_just_pressed("right_click"):
+	if Input.is_action_just_pressed("right_click") || Input.is_action_just_pressed("middle_mouse_click"):
 		
 		mouse_position = get_viewport().get_mouse_position()
 	
-	if event is InputEventMouseMotion && Input.is_action_pressed("right_click"):
+	if event is InputEventMouseMotion && (Input.is_action_pressed("right_click") || Input.is_action_just_pressed("middle_mouse_click")):
 		
 		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 		rotate_y(deg_to_rad(-event.relative.x * mouse_sensitivity))
 		$BeanModel.rotate_y(deg_to_rad(event.relative.x * mouse_sensitivity))
 		camera_pivot_x.rotate_x(deg_to_rad(event.relative.y * mouse_sensitivity))
-	
-	if Input.is_action_just_released("right_click"):
+
+	if Input.is_action_just_released("right_click") || Input.is_action_just_released("middle_mouse_click"):
 		
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 		Input.warp_mouse(mouse_position)
@@ -35,7 +35,7 @@ func _input(event: InputEvent) -> void:
 
 func _physics_process(delta):
 	var direction:Vector3 = Vector3.ZERO
-
+	
 	if Input.is_action_pressed("D_input"):
 		direction.x += 1
 	if Input.is_action_pressed("A_input"):
@@ -47,16 +47,18 @@ func _physics_process(delta):
 		
 	if Input.is_action_pressed("space_press"):
 		direction.y += 1
-
+	
+	camera_pivot_x.rotation_degrees.x = clampf(camera_pivot_x.rotation_degrees.x, -75, 50.0)
+	
 	# Ground Velocity
 	target_velocity.x = direction.x * speed
 	target_velocity.z = direction.z * speed
 	target_velocity.y += direction.y * speed
-
+	
 	# Vertical Velocity
 	if !is_on_floor(): # If in the air, fall towards the floor. Literally gravity
 		target_velocity.y = target_velocity.y - (fall_acceleration * delta)
-
+	
 	# Moving the Character
 	velocity = target_velocity
 	move_and_slide()
